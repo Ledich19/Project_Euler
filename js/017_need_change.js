1a03414286b7e2c793d500682ceaef94c431d34a
Aleksandr <ledich19@gmail.com>
2021-05-22 23:16:09 +0300

    27

--- js/017_need_change.js
+++ js/017_need_change.js
@@ -1,4 +1,11 @@
 'use strict'
+// Если записать числа от 1 до 5 английскими словами (one, two, three, four, five), 
+// то используется всего 3 + 3 + 5 + 4 + 4 = 19 букв.
+// Сколько букв понадобится для записи всех чисел от 1 до 1000 (one thousand) включительно?
+// Примечание: Не считайте пробелы и дефисы. Например, число 342 (three hundred and forty-two)
+//  состоит из 23 букв, число 115 (one hundred and fifteen) - из 20 букв.
+//   Использование "and" при записи чисел соответствует правилам британского английского.
+
 const arr1To9 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 const arr10To19 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 const arr20To90 = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
