d79ca584604c1c595b601f5d60431b811c1913d8
Aleksandr <ledich19@gmail.com>
2024-09-12 22:41:18 +0300

    feat: add 046

---
+++ js/utils.ts
@@ -0,0 +1,15 @@
+export const isSimple = (value: number) => {
+  for (let i = 2; i <= Math.sqrt(value); i++) {
+    if (value % i === 0) return false;
+  }
+  return value > 1;
+};
+
+export const generateSimpleNumbers = (value: number, numbers: number[]) => {
+  const last = numbers.at(-1) || 0;
+  for (let i = last + 1; i <= value; i++) {
+    if (isSimple(i)) {
+      numbers.push(i);
+    }
+  }
+};
